3106f1fbe1b926fcb89505907aef09b1a38ac725
niqdev <niqdev@gmail.com>
2016-12-21 18:41:25 +0000

    lib: draft mock tests

--- lib/src/test/scala/com/github/niqdev/myidmobile/MyIdMobileBrowserSpec.scala
+++ lib/src/test/scala/com/github/niqdev/myidmobile/MyIdMobileBrowserSpec.scala
@@ -5,21 +5,27 @@ import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 class MyIdMobileBrowserSpec extends FlatSpec with Matchers with MockFactory {
 
-  "Login" should "invoke GET with config url" in {
+  it should "test" in {
     val configStub = stub[Config]
     configStub.getString _ when "url.login" returns "urlLogin"
 
-    val jsoupBrowserMock = mock[JsoupBrowser]
-    jsoupBrowserMock.get _ expects "urlLogin"
-    //jsoupBrowserMock.get _ when "urlLogin" returns JsoupDocument(new org.jsoup.nodes.Document(""))
+    val jsoupBrowserStub = stub[JsoupBrowser]
+    jsoupBrowserStub.get _ when  "urlLogin" returns JsoupBrowser().parseResource("/html/myid-mobile-ie-login-get.html")
 
     val myIdCredential = MyIdCredential(PhonePrefix.IE, "MyMobileNumber", "MyPassword")
-    val myIdMobile = new MyIdMobile(myIdCredential, configStub, jsoupBrowserMock)
-    myIdMobile.getLogin map { document => assert(document == "")}
+    val myIdMobile = new MyIdMobile(myIdCredential, configStub, jsoupBrowserStub)
+
+    val login = myIdMobile.getLogin andThen {
+      case document => println(document)
+    }
+
+    Await.ready(login, Duration.Inf)
   }
 
 }
